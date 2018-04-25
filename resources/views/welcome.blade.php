f357b9893448087cf8f447ce3925c3976216b0c6
Arun Babu N <ab@ameyem.com>
2018-04-25 16:39:32 +0530

    animation added3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,8 +18,8 @@
 
 </style>
 
-{{--  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>  --}}
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <div class="container-fluid">
         <nav class="navbar transparent">
             <div class="navbar-inner">
